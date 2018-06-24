4d42d7e7fcd31140984aee2995f7401692be6019
Jesse McReady <jessemcready@gmail.com>
2018-06-24 16:43:06 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,7 +16,7 @@ def first_challenge
   #your code here
   contacts["Freddy Mercury"].each do |key, value|
     if key == :favorite_icecream_flavors
-      
+      contacts["Freddy Mercury"][key].delete_if { value == "strawberry" }
     end
   end
 
